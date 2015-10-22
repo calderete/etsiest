3133b12313900d41776c906b46d78575b9653b26
Charles <codprestige10@gmail.com>
2015-10-22 15:13:07 -0400

    skeleton files

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source 'https://rubygems.org'
+
+# Specify your gem's dependencies in etsiest.gemspec
+gemspec
+
+gem "etsy"
+gem "sinatra"
+
+group :development  do
+	gem "pry"
+end
---
+++ lib/etsiest.rb
@@ -0,0 +1,11 @@
+require "etsiest/version"
+require "sinatra/base"
+require "etsy"
+require "pry"
+require "estiest/version"
+
+
+module Etsiest
+	class App < Sinatra::Base
+  # Your code goes here...
+end
