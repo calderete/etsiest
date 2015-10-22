86a61c72bf210d447749d1500e9a858ab9975ace
Charles <codprestige10@gmail.com>
2015-10-22 18:38:06 -0400

    sinatra is getting input from the web page

--- lib/etsiest.rb
+++ lib/etsiest.rb
@@ -2,10 +2,24 @@ require "etsiest/version"
 require "sinatra/base"
 require "etsy"
 require "pry"
-require "estiest/version"
+require "etsiest/version"
 
-
+Etsy.api_key = ENV['ETSY_KEY']
 module Etsiest
-	class App < Sinatra::Base
-  # Your code goes here...
+	class Search < Sinatra::Base
+		set :logging, true
+      
+
+      get "/search" do
+      item = params["query"]
+      listings = Etsy::Request.get('listings/active', :includes => ['Images', 'Shop'], 
+      									   :keywords => "item")
+      erb :index, locals: { query: params["query"]}
+  	  
+  	  end
+
+  	  run! if app_file == $0
+  	 
+  	end
+
 end
