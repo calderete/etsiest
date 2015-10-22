591566fd4cfe758b07edad9d2a4ade4f2f3a78dc
Charles <codprestige10@gmail.com>
2015-10-22 19:59:48 -0400

    I think I'm getting somewere with this, getting used to the gem

--- lib/etsiest.rb
+++ lib/etsiest.rb
@@ -4,17 +4,19 @@ require "etsy"
 require "pry"
 require "etsiest/version"
 
-Etsy.api_key = ENV['ETSY_KEY']
+
 module Etsiest
 	class Search < Sinatra::Base
+		include Etsy
 		set :logging, true
-      
+      	set :auth_key, "#{ENV["AUTH_KEY"]}"
 
-      get "/search" do
-      item = params["query"]
-      listings = Etsy::Request.get('listings/active', :includes => ['Images', 'Shop'], 
-      									   :keywords => "item")
-      erb :index, locals: { query: params["query"]}
+      get "/" do
+      Etsy.api_key = "#{settings.auth_key}"
+      list = Etsy::Request.get('/listings/active', :includes => ['Images', 'Shop'], 
+      									   :keywords => 'whiskey')
+      binding.pry
+      erb :index, locals: { items: @data }
   	  
   	  end
 
