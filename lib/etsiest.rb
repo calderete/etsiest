5f03eb7b4f69319f9523d6f8865c6bf0a0715cdf
Charles <codprestige10@gmail.com>
2015-10-23 13:13:37 -0400

    got images to pull up

--- lib/etsiest.rb
+++ lib/etsiest.rb
@@ -2,22 +2,35 @@ require "etsiest/version"
 require "sinatra/base"
 require "etsy"
 require "pry"
-require "etsiest/version"
+
 
 
 module Etsiest
 	class Search < Sinatra::Base
-		include Etsy
+		#include Etsy
 		set :logging, true
-      	set :auth_key, "#{ENV["AUTH_KEY"]}"
+    set :auth_key, "#{ENV["AUTH_KEY"]}"
 
       get "/search" do
       Etsy.api_key = "#{settings.auth_key}"
-      list = Etsy::Request.get('/listings/active', :find => ['Images', 'Shop'], 
+      list = Etsy::Request.get('/listings/active', :includes => ['Images', 'Shop'], 
       									   :keywords => 'whiskey')
+      @image_list = []
+      response = list.result
+      response.each do |i|
+        binding.pry
+        item = { 
+          line: i["Images"]}
+          #binding.pry
+          item[:line].each do |p|
+            pic = p["url_fullxfull"]
+        @image_list.push(pic)
+
+        end
+      end
       
-      binding.pry
-      erb :index, locals: { items: list.result }
+      
+      erb :index, locals: { items: @image_list }
   	  
   	  end
 
