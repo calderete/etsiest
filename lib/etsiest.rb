f11f0344f8bdf10a260d976927d3c1c99100eaf9
Charles <codprestige10@gmail.com>
2015-10-22 20:30:36 -0400

    Got some json back, now to drill into it

--- lib/etsiest.rb
+++ lib/etsiest.rb
@@ -11,12 +11,12 @@ module Etsiest
 		set :logging, true
       	set :auth_key, "#{ENV["AUTH_KEY"]}"
 
-      get "/" do
+      get "/search" do
       Etsy.api_key = "#{settings.auth_key}"
       list = Etsy::Request.get('/listings/active', :includes => ['Images', 'Shop'], 
       									   :keywords => 'whiskey')
       binding.pry
-      erb :index, locals: { items: @data }
+      erb :index, locals: { items: list.result }
   	  
   	  end
 
